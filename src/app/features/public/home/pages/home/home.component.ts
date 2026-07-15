e842a734cfbdbffa93eb891501876c47a2e5d0eb
Juan Francisco <juan.maldonado.leon@gmail.com>
2026-07-15 18:16:59 -0400

    feat: agrega barra superior

--- src/app/features/public/home/pages/home/home.component.ts
+++ src/app/features/public/home/pages/home/home.component.ts
@@ -1,11 +1,11 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { ListaPlaylistsComponent } from '../../components/lista-playlists/lista-playlists.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterOutlet, ListaPlaylistsComponent],
+  imports: [RouterOutlet, RouterLink, ListaPlaylistsComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
